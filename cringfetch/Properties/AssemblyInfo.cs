6e37835c1abd900bbafbe370bfab23789f554f01
Mrkod's Software Solutions <mrkodomoh64@hotmail.com>
2021-12-05 18:36:27 +0300

    version

--- cringfetch/Properties/AssemblyInfo.cs
+++ cringfetch/Properties/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("cringfetch")]
 [assembly: AssemblyDescription("As seen on Linux!")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("MSS")]
+[assembly: AssemblyCompany("MKware")]
 [assembly: AssemblyProduct("cringfetch")]
-[assembly: AssemblyCopyright("Copyright (c) 2021, Mrkod's Software Solutions. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright (c) 2021, MKware. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.0.1")]
-[assembly: AssemblyFileVersion("1.1.0.1")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
